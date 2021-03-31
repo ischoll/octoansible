6467184c25d59f374f5410a0fb8e3b913ced669c
ian <ischoll@me.com>
2021-03-31 13:40:04 -0400

    Reverting now works

--- revertingsnapshot.ps1
+++ revertingsnapshot.ps1
@@ -1,6 +1,14 @@
+
+$vc="10.0.0.197"
+
+Connect-VIServer -Server $vc
+
+
 foreach($vm in Get-Folder 555varnish | Get-VM){
 	$snap = Get-Snapshot -VM $vm | Sort-Object -Property Created -Descending | Select -First 1
 	Set-VM -VM $vm -SnapShot $snap -Confirm:$false
 }
+
+Disconnect-Viserver * -Confirm:$false
 
 
